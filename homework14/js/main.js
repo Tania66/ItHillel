91112b29345ae01f708a06dcefbb83c56af61510
Tania <113130993+Tania66@users.noreply.github.com>
2024-06-29 20:34:04 +0300

    slider_1

--- homework14/js/main.js
+++ homework14/js/main.js
@@ -1,12 +1,57 @@
+const images = [
+  {
+    src: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+  },
+  {
+    src: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+  },
+  {
+    src: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+  },
+  {
+    src: "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
+  },
+  {
+    src: "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
+  },
+  {
+    src: "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
+  },
+  {
+    src: "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
+  },
+  {
+    src: "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
+  },
+];
 const slider = document.querySelector(".slider");
-const allSlides = slider.querySelectorAll("img");
-const allDots = document.querySelectorAll(".dot");
+const dotsContainer = document.querySelector(".slider-dots");
 const prevBtn = document.querySelector(".prev-button");
 const nextBtn = document.querySelector(".next-button");
-const sliderCount = allSlides.length;
+const sliderCount = images.length;
 let indexSlide = 0;
 
+function createImgSlides() {
+  images.forEach((element) => {
+    const slide = document.createElement("img");
+    slide.classList.add("images");
+    slide.src = element.src;
+    slider.append(slide);
+  });
+}
+createImgSlides();
+
+function createDots() {
+  for (let i = 1; i <= sliderCount; i++) {
+    let spanEl = document.createElement("span");
+    spanEl.classList.add("dots-item");
+    dotsContainer.append(spanEl);
+  }
+}
+createDots();
+
 function updateSlide() {
+  const allSlides = slider.querySelectorAll("img");
   allSlides.forEach((element, index) => {
     if (index === indexSlide) {
       element.style.display = "block";
@@ -26,7 +71,6 @@ function updateSlide() {
     }
   });
 }
-
 updateSlide();
 
 const handleNextClick = () => {
@@ -34,13 +78,14 @@ const handleNextClick = () => {
   currentSlideDot(indexSlide);
   updateSlide();
 };
-
 const handlePrevClick = () => {
   indexSlide = (indexSlide - 1) % sliderCount;
   currentSlideDot(indexSlide);
-
   updateSlide();
 };
+
+const allDots = document.querySelectorAll(".dots-item");
+allDots[0].classList.add("active-dot");
 
 function currentSlideDot(indexSlide) {
   allDots.forEach((element, index) => {
